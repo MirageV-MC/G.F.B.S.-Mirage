4aa0521f9b4f380c2595c5dc6f0ec23059e6d508
Convex89524 <121009065+Convex89524@users.noreply.github.com>
2026-03-16 05:59:44 +0800

    前面忘了后面忘了反正修了一堆.

--- src/main/java/org/mirage/gfbs/api/CountdownAPI.java
+++ src/main/java/org/mirage/gfbs/api/CountdownAPI.java
@@ -1,3 +1,21 @@
+/**
+ * G.F.B.S. Mirage (mirage_gfbs) - A Minecraft Mod
+ * Copyright (C) 2025-2029 Mirage-MC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.
+ */
+
 package org.mirage.gfbs.api;
 
 import net.minecraft.server.level.ServerPlayer;
@@ -7,23 +25,6 @@ import org.mirage.gfbs.Tools.CountdownPopup.CountdownServerManager;
 import org.mirage.gfbs.Tools.CountdownPopup.CountdownStopS2CPacket;
 import org.mirage.gfbs.Tools.CountdownPopup.ModNetworking;
 
-/**
- * G.F.B.S. Mirage (mirage_gfbs) - A Minecraft Mod
- * Copyright (C) 2025-2029 Mirage-MC
- * <p>
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * <p>
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * <p>
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
 public final class CountdownAPI {
 
     private CountdownAPI() {}
